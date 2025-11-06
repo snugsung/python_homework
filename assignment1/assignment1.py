a98b6a2ccf3cd1fca042cdb74230949f9a5dacfe
Sung Kim <stwkim103@gmail.com>
2025-11-06 13:44:54 -0500

    Complete Task 3 Part 1

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -9,3 +9,21 @@ def greet(name):
     return (f"Hello, {name}!")
 
 print(greet("Sung"))
+
+# Task 3
+def calc(a, b, operation="multiply"):
+    match operation: 
+        case "add": 
+            a + b
+        case "subtract":
+            a - b 
+        case "multiply": 
+            a * b
+        case "divide":
+            a / b
+        case "modulo":
+            a % b
+        case "int_divide":
+            a // b
+        case "power":
+            a ** b
